53c5e993c52a4e013fe18b708fdfc1aaf7c895f0
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-09-23 18:01:49 +0200

    FastAPI in a nutshell

---
+++ odoo_fastapi_demo/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2021 ACSONE SA/NV
+
+"""Using the Odoo ORM with FastAPI.
+
+This is the example repository accompanying the Odoo XP 2021 talk.
+"""
+
+from .app import app
+
+__version__ = "0.1"
---
+++ odoo_fastapi_demo/app.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2021 ACSONE SA/NV
+
+from typing import Optional, List
+
+from fastapi import FastAPI, HTTPException
+from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
+from pydantic import BaseModel
+
+app = FastAPI(title="FastAPI Demo")
+
+
+class Partner(BaseModel):
+    id: Optional[int]
+    name: str
+    email: Optional[str]
+    is_company: bool = False
+
+
+tom = Partner(id=1, name="Tom", email="tom@wb.com")
+
+
+@app.get("/partners", response_model=List[Partner])
+def partners():
+    return [
+        tom,
+        Partner(id=2, name="Jerry", email="jerry@wb.com"),
+        dict(id=3, name="Warner Bros", email="info@wb.com", is_company=True),
+    ]
+
+
+@app.get("/partners/{partner_id}", response_model=Partner)
+def get_partner(partner_id: int):
+    if partner_id == 1:
+        return tom
+    raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+
+
+@app.post("/partners", response_model=Partner, status_code=HTTP_201_CREATED)
+def create_partner(partner: Partner):
+    return partner
