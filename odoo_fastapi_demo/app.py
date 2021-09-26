1fe757ff48301310edc23ef0018e56ccf67b76f0
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-09-26 12:45:10 +0200

    Control the thread pool size

--- odoo_fastapi_demo/app.py
+++ odoo_fastapi_demo/app.py
@@ -17,6 +17,16 @@ app = FastAPI(title="FastAPI with Odoo Demo")
 
 
 @app.on_event("startup")
+def set_default_executor() -> None:
+    from concurrent.futures import ThreadPoolExecutor
+    import asyncio
+
+    loop = asyncio.get_running_loop()
+    # Tune this according to your requirements !
+    loop.set_default_executor(ThreadPoolExecutor(max_workers=5))
+
+
+@app.on_event("startup")
 def initialize_odoo() -> None:
     # Read Odoo config from $ODOO_RC.
     odoo.tools.config.parse_config([])
